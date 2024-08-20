396de4cf8944e720f0de22a7e76da11273b67d73
vivilu <viviana.recalde@gmail.com>
2024-08-20 15:46:13 -0500

    Inicio de proyecto
    Previo a vercel y bdd postgres

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+/** esto importa los estilos globales */
+import '@/app/ui/global.css';
+import { roboto } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
